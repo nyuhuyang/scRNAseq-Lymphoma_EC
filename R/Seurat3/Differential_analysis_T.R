63803f037f6d29e523fb181282c173beb288fa2b
Yang Hu <nyuhuyang@gmail.com>
2019-09-26 11:31:02 -0400

    Add TALL DE, Venn_Diagrams, VolcanoPlots, gsea

---
+++ R/Seurat3/Differential_analysis_T.R
@@ -0,0 +1,72 @@
+########################################################################
+#
+#  0 setup environment, install libraries if necessary, load libraries
+# 
+# ######################################################################
+
+library(Seurat)
+library(dplyr)
+library(tidyr)
+library(kableExtra)
+library(gplots)
+library(MAST)
+source("../R/Seurat3_functions.R")
+path <- paste0("output/",gsub("-","",Sys.Date()),"/")
+if(!dir.exists(path))dir.create(path, recursive = T)
+#3.1  Compare DE across all major cell types==================
+#We would need the data for all clusters, as well the subclusters.
+#detect changes in gene expression between young and aged, 
+#in the different cell types and subtypes. 
+#It will also be interesting to check if there is some subtype enriched in young compared to aged or viceversa.
+
+# SLURM_ARRAY_TASK_ID
+slurm_arrayid <- Sys.getenv('SLURM_ARRAY_TASK_ID')
+if (length(slurm_arrayid)!=1)  stop("Exact one argument must be supplied!")
+# coerce the value to an integer
+args <- as.numeric(slurm_arrayid)
+print(paste0("slurm_arrayid=",args))
+
+# 3.1.1 load data
+(load(file="data/Lymphoma_EC_10_20190922.Rda"))
+Idents(object) <-  "Doublets"
+object %<>% subset(idents = "Singlet")
+
+# subset date
+Idents(object) = "date"
+(dates <- unique(object$date))
+(date <- dates[args])
+object %<>% subset(idents = date)
+
+# Rename ident
+Idents(object) <-  "integrated_snn_res.0.6"
+object %<>% RenameIdents("0" = "T cells",
+                         "1" = "Endothelial cells",
+                         "2" = "T cells",
+                         "3" = "T cells",
+                         "4" = "Endothelial cells",
+                         "5" = "T cells",
+                         "6" = "T cells",
+                         "7" = "T cells",
+                         "8" = "Endothelial cells",
+                         "9" = "T cells",
+                         "10" = "T cells")
+object@meta.data$cell.type = as.character(Idents(object))
+# subset cell types
+object@meta.data$cell.type_conditions = paste0(object@meta.data$cell.type,"_",
+                                         object@meta.data$conditions)
+Idents(object) = "cell.type_conditions"
+object %<>% subset(idents = c("T cells_T-ALL","T cells_EC+T-ALL"))
+object %<>% RenameIdents("T cells_T-ALL" = "T-ALL",
+                         "T cells_EC+T-ALL" = "T-ALL+EC")
+object@meta.data$cell.type_conditions = as.character(Idents(object))
+Idents(object) = "cell.type_conditions"
+object %<>% sortIdent
+table(Idents(object))
+T_cells_markers <- FindMarkers.UMI(object, assay = "RNA",
+                              ident.1 = "T-ALL+EC", ident.2 = "T-ALL",
+                              #return.thresh = 1,
+                              test.use = "MAST",min.pct = -Inf,
+                              min.cells.feature = -Inf, min.cells.group = -Inf,
+                              only.pos = F, # don't change it!
+                              logfc.threshold = -Inf)
+write.csv(T_cells_markers,paste0(path,"T_cells_markers_",date,".csv"))
--- R/Seurat3/fgsea.R
+++ R/Seurat3/fgsea.R
@@ -20,11 +20,14 @@ if(!dir.exists(path)) dir.create(path, recursive = T)
 # 3.1.1 load data
 dates <- c("2018-10-18","2018-12-30")
 cluster <- c("EC+RO2", "EC+3119")
+cell.type = c("EC","TALL")
+k=1
 res <- list()
 for(i in seq_along(c("2018-10-18","2018-12-30"))){
-        res[[i]] = read.csv(file=paste0("output/20190924/EC_markers_",dates[i],".csv"),
-                       stringsAsFactors=F)
-        res[[i]] = res[[i]][res[[i]]$cluster %in% "TEC",]
+        res[[i]] = read.csv(file=paste0("output/20190924/",cell.type[k],"_markers_",dates[i],".csv"),
+                            row.names = 1,stringsAsFactors=F)
+        #res[[i]] = res[[i]][res[[i]]$cluster %in% "TEC",]
+        res[[i]]$gene = rownames(res[[i]])
         res[[i]]$cluster = cluster[i]
 }
 
@@ -49,79 +52,23 @@ names(hallmark) = gsub("_"," ",names(hallmark))
 names(allpathways) = gsub("_"," ",names(allpathways))
 
 hallmark_fgesa <- FgseaDotPlot(stats=res, pathways=hallmark, nperm=1000,
-             avg_logFC =0.001 ,padj = 0.25,pval = 0.05,
+             avg_logFC =0.01 ,padj = 0.25,pval = 0.05,
              order.by = c("EC+RO2","NES"),decreasing = F,
              size = "-log10(pval)", fill = "NES",do.return = T,
-             sample = "Educated endothelial cells", 
+             sample = paste("Educated",cell.type[k]), 
              pathway.name = "Hallmark",rotate.x.text = T,
              font.xtickslab=14, font.main=18,font.ytickslab = 10,
              font.legend = list(size = 14),font.label = list(size = 14),
-             width=7, height=7,hjust=0.8)
-write.csv(hallmark_fgesa,paste0(path,"hallmark_fgesa.csv"))
+             width=8, height=7,hjust=0.8)
+write.csv(hallmark_fgesa,paste0(path,cell.type[k],"_hallmark_fgesa.csv"))
 
 allpathways_fgesa <- FgseaDotPlot(stats=res, pathways=allpathways, nperm=1000,
-             avg_logFC =0.05 ,padj = 0.2,pval = 0.05,
+             avg_logFC =0.01 ,padj = 0.25,pval = 0.05,
              order.by = c("EC+RO2","NES"),decreasing = F,
              size = "-log10(pval)", fill = "NES",do.return = T,
-             sample = "Educated endothelial cells", 
+             sample = paste("Educated",cell.type[k]), 
              rotate.x.text = T, pathway.name = "Hallmark, biocarta,and KEGG",
-             font.xtickslab=14, font.main=12,font.ytickslab = 10,
+             font.xtickslab=14, font.main=12,font.ytickslab = 8,
              font.legend = list(size = 12),font.label = list(size = 12),
              width=6, height=7,hjust=0.67)
-write.csv(allpathways_fgesa,paste0(path,"allpathways_fgesa.csv"))
-
-df_samples <- readxl::read_excel("doc/190626_scRNAseq_info.xlsx")
-colnames(df_samples) <- tolower(colnames(df_samples))
-groups = c("Untreated","Pt-17","Pt-25")
-for(i in 1:length(groups)){
-        res_B = read.csv(file = paste0("output/20190622/B/B_MCL_DE/B_",groups[i],".csv"))
-        res_T = read.csv(file = paste0("output/20190622/T/T_NK_DE/T_",groups[i],".csv"))
-        
-        (samples = df_samples$sample[df_samples$sample %in% unique(res_B$cluster)])
-        #res_B$cluster %<>% factor(levels = samples)
-        #res_T$cluster %<>% factor(levels = samples)
-        
-        FgseaDotPlot(stats=res_B, pathways=hallmark, nperm=1000,padj = 0.25,pval = 0.05,
-                     order.by = c(4,"NES"),decreasing = F,
-                     size = "-log10(pval)", fill = "NES",
-                     sample = paste(groups[i],"B_MCL clusters"), 
-                     pathway.name = "Hallmark",rotate.x.text = F)
-        (samples = df_samples$sample[df_samples$sample %in% unique(res_T$cluster)])
-        FgseaDotPlot(stats=res_T, pathways=hallmark, nperm=1000,padj = 0.25,pval = 0.05,
-                     order.by = c(4,"NES"),decreasing = F,
-                     size = "-log10(pval)", fill = "NES",
-                     sample = paste(groups[i],"T_NK clusters"), 
-                     pathway.name = "Hallmark",rotate.x.text = F)
-
-}
-
-#============ T cells  =====================
-df_samples <- readxl::read_excel("doc/190626_scRNAseq_info.xlsx")
-colnames(df_samples) <- tolower(colnames(df_samples))
-tests <- paste0("test",4)
-sample_n = which(df_samples$tests %in% tests)
-df <- as.data.frame(df_samples[sample_n,])
-(samples <- c("Normal",unique(df$sample)))
-cell.type <- c("T_cells:CD4+","T_cells:CD8+","NK_cells")
-
-res_T_list <- list()
-for(i in 2:length(samples)){
-        res_T_list[[i-1]] = read.csv(file = paste0(path,samples[i],"_vs_Normal",".csv"))
-}
-res_T <- do.call("rbind.data.frame", res_T_list)
-res_T = res_T[grep("CD8+",res_T$cluster1.vs.cluster2),]
-res_T$cluster1.vs.cluster2 %<>% as.character %>% gsub('\\..*',"",.)
-
-res_T$cluster1.vs.cluster2 %<>% factor(levels = samples[2:5])
-        
-for(i in 2:length(samples)) FgseaBarplot(pathways=hallmark, stats=res_T, nperm=1000,
-                                          cluster = samples[i],no.legend = F,
-                                          cut.off = "padj",cut.off.value = 0.25,
-                                          sample="CD8+ T cells of",pathway.name = "Hallmark", hjust=0.5,
-                                          width=10, height=7)
-
-FgseaDotPlot(stats=res_T, pathways=hallmark, nperm=1000,padj = 0.25,pval = 0.05,
-                     order.by = c(4,"NES"),decreasing = F,
-                     size = "-log10(pval)", fill = "NES",
-                     sample = paste("Pt-17's CD8+ T cells"), 
-                     pathway.name = "Hallmark",rotate.x.text = F)
+write.csv(allpathways_fgesa,paste0(path,cell.type[k],"allpathways_fgesa.csv"))
